4720d6841668fc1f643637be620032573f83467e
David Padilla <david@easybroker.com>
2015-02-11 01:27:55 -0600

    Polish the Rakefile

--- test/unit/alacena/request_test.rb
+++ test/unit/alacena/request_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Alacena::RequestTest < MiniTest::Unit::TestCase
+class Alacena::RequestTest < MiniTest::Test
   def test_parse_method
     data = "POST /record/ HTTP/1.1\r\nAccept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3\r\nAccept: */*\r\nUser-Agent: Ruby\r\nConnection: close\r\nHost: 127.0.0.1:8080\r\nContent-Length: 20\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nkey=some-key&value=1"
     @request = Alacena::Request.new(data)
